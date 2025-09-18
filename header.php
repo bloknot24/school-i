f991da42c3c4e732a9977d49c72446f739081df9
Alex Ignatovich <alex_ignatovich@vk.com>
2025-09-18 22:03:34 +0300

    New edits all files

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <title><?= $title; ?></title>
     <meta name="description" content="<?= $description; ?>">
     <meta property="og:title" content="Первый Я">
-    <meta property="og:description" content="Первый Я - объединение участников и наставников, для роста поколения, способного создать достойное будущее для себя и счастливую жизнь для всех поколений страны.">
+    <meta property="og:description" content="<?= $description; ?>">
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <link rel="stylesheet" href="css/normalize.css">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
