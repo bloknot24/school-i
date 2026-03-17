a4a73280d36877b611c744167554e9198d352b74
Alex Ignatovich <alex_ignatovich@vk.com>
2026-03-17 14:53:31 +0300

    Add new code yandex.metrika

--- footer.php
+++ footer.php
@@ -329,8 +329,6 @@
     </div>
 </div>
 
-
-
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/jquery.arcticmodal-0.3.min.js"></script>
 <script src="js/arctikmodal.js"></script>
@@ -343,9 +341,8 @@
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
     (function(m, e, t, r, i, k, a) {
-        m[i] = m[i]
-        function() {
-            (m[i].a = m[i].a[]).push(arguments)
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
         };
         m[i].l = 1 * new Date();
         for (var j = 0; j < document.scripts.length; j++) {
